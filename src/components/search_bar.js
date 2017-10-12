4e3ae2d90dd09e47a9a973766e635336e176313d
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-10-12 09:53:40 -0700

    Add Css styling to the app and adjust video list and make all the videos clickable

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,10 +9,15 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
-        <input onChange={event => this.setState({ term: event.target.value })} />
+      <div className="search-bar">
+        <input onChange={event => this.onInputChange(event.target.value) } />
       </div>
     );
+  }
+
+  onInputChange(term){
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
 import VideoList from './components/video_list';
+import VideoListItem from './components/video_list_item';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyBYDnssmFFOKnoUT-vg6wC8d492gjORXdM';
@@ -9,18 +11,31 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
-      //this.setState({ videos: videos });
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term){
+    YTSearch({ key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
     });
   }
+
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+        videos={this.state.videos} />
       </div>
     );
   }
